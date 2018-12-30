8a2ffcb12b5d84ab99d4fc851dc0add722e80f5f
Somashekhar2302 <spattanshetty509@gmail.com>
2018-12-30 21:10:48 +0530

    first commit

---
+++ drive/Website/myWebsite/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialComponent } from './social/social.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ConfigService} from './config.service';
+import { BlogComponent } from './blog/blog.component';
+import { PostComponent } from './post/post.component';
+import { ArticleComponent } from './article/article.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroComponent,
+    GalleryComponent,
+    ContentComponent,
+    TestimonialComponent,
+    FooterComponent,
+    ClientsComponent,
+    PricingComponent,
+    HeaderComponent,
+    SocialComponent,
+    NavigationComponent,
+    BlogComponent,
+    PostComponent,
+    ArticleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [ConfigService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ drive/Website/myWebsite/src/app/configuration.ts
@@ -0,0 +1,107 @@
+export const configuration = {
+header : {
+    heading: 'My Website',
+    headingtext: "Namari is a free landing page template you can use for your projects. It is free to use for your personal and commercial projects, enjoy!",
+    buttontext: "Action",
+    buttonlink: '/Home'
+  },
+  intro : {
+    tagline : 'SUCCESS',
+    title : 'How We Help You To Sell Your Product',
+    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!',
+    features : [
+      {icon:'html5', title:'HTML5 &amp; CSS3', description:'Has ne tritani atomorum conclusionemque, in dolorum volumus cotidieque eum. At vis choroneglegentur iudico'},
+      {icon:'bolt', title:'Easy to Use', description:'Cu vero ipsum vim, doctus facilisi sea in. Eam ex falli honestatis repudiandae, sidetracto mediocrem disputationi'},
+      {icon:'tablet', title:'Fully Responsive', description:'Id porro tritani recusabo usu, eum intellegam consequuntur et. Fugit debet ea sit, an pronemore vivendum'},
+      {icon:'rocket', title:'Parallax Effect', description:'Id porro tritani recusabo usu, eum intellegam consequuntur et. Fugit debet ea sit, an pronemore vivendum'}]
+  },
+  services : {
+    tagline : 'BELIEVING',
+    title : 'Focusing On What Matters Most',
+    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!'
+  },
+  testimonials : {
+    tagline : 'FEEDBACK',
+    title : 'What our customers are saying',
+    description : '',
+    feedbacks : [
+      {name:'John Doe', userimage:'user-1.jpg', comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' , company: 'Happy Customer'},
+      {name:'Roslyn Doe', userimage:'user-2.jpg', comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' , company: 'Happy Customer'},
+      {name:'Thomas Doe', userimage:'user-3.jpg', comments:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua' , company: 'Happy Customer'},
+    ]
+  },
+  clients : {
+    tagline : 'TRUST',
+    title : 'Companies who use our services',
+    description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!',
+    companies : [
+      {name:'Tree', weblink:'company-logo1.png', logo:'company-logo1.png'},
+      {name:'Fingerprint', weblink:'company-logo2.png', logo:'company-logo2.png'},
+      {name:'The Man', weblink:'company-logo3.png', logo:'company-logo3.png'},
+      {name:'Mustache', weblink:'company-logo4.png', logo:'company-logo4.png'},
+      {name:'Goat', weblink:'company-logo5.png', logo:'company-logo5.png'},
+      {name:'Jusitce', weblink:'company-logo6.png', logo:'company-logo6.png'},
+      {name:'Ball', weblink:'company-logo7.png', logo:'company-logo7.png'},
+      {name:'Cold', weblink:'company-logo8.png', logo:'company-logo8.png'},
+      {name:'Apple', weblink:'company-logo9.png', logo:'company-logo9.png'}
+    ]
+  },
+  pricing : {
+    tagline : 'YOUR CHOICE',
+    title : 'We have the right package for you',
+    description : '',
+    plans:[
+      {title:'PERSONAL', subtitle:'The standard version', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit', price:'19', currency:'$', 
+      downloads:'5 Downloads',extensions:'2 Extensions',tutorials:'Tutorials',support:'Forum Support',updates:'1 year free updates',
+      buttontext:'Buy Now', buttonlink:'#', featured: true
+    },
+    {title:'STUDENT', subtitle:'Most popular choice', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit', price:'29', currency:'$', 
+      downloads:'15 Downloads',extensions:'5 Extensions',tutorials:'Tutorials with files',support:'Forum Support',updates:'2 year free updates',
+    buttontext:'Buy Now', buttonlink:'#', featured: true
+  },
+  {title:'BUSINESS', subtitle:'For the whole team', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit', price:'49', currency:'$',
+    downloads:'Unlimited Downloads',extensions:'Unlimited Extensions',tutorials:'HD Video Tutorials',support:'Chat Support',updates:'lifetime free updates',
+  buttontext:'Buy Now', buttonlink:'#', featured: true
+}
+    ]
+  },
+  gallery : {
+    images:[
+      'gallery-image-1.jpg',
+      'gallery-image-2.jpg',
+      'gallery-image-3.jpg',
+      'gallery-image-4.jpg',
+      'gallery-image-5.jpg',
+      'gallery-image-6.jpg',
+    ]
+  },
+  footer: {
+      copyrightText: 'Made with code',
+      developer: 'Benki',
+      developerlink: ''
+  },
+  socialsites:[
+    {title:'Facebook', target:'_blank', username:'benki', icon:'facebook' },
+    {title:'Google+', target:'_blank', username:'benki', icon:'google-plus' },
+    {title:'Twitter', target:'_blank', username:'benki', icon:'twitter' },
+    {title:'Instagram', target:'_blank', username:'benki', icon:'instagram' },
+    {title:'behance', target:'_blank', username:'benki', icon:'behance' },
+  ],
+  blog:{
+    tagline : 'My Blog',
+    title : 'Rocky',
+    posts:[
+      {id: 1, title:'the first author', author:'AD',image:'gallery-image-1.jpg', publishdate:'2019-01-19T07:22Z', excert :'This is the summary'},
+      {id: 2, title:'the second author', author:'AD',image:'gallery-image-2.jpg', publishdate:'2019-01-19T07:22Z', excert :'This is the summary'},
+      {id: 3, title:'the third author', author:'AD',image:'gallery-image-3.jpg', publishdate:'2019-01-19T07:22Z', excert :'This is the summary'},
+      {id: 4, title:'the forth author', author:'AD',image:'gallery-image-4.jpg', publishdate:'2019-01-19T07:22Z', excert :'This is the summary'},
+      {id: 5, title:'the fifth author', author:'AD',image:'gallery-image-5.jpg', publishdate:'2019-01-19T07:22Z', excert :'This is the summary'},
+      {id: 6, title:'the sixth author', author:'AD',image:'gallery-image-6.jpg', publishdate:'2019-01-19T07:22Z', excert :'This is the summary'},
+      {id: 7, title:'the seventh author', author:'AD',image:'gallery-image-1.jpg', publishdate:'2019-01-19T07:22Z', excert :'This is the summary'},
+      {id: 8, title:'the eight author', author:'AD',image:'gallery-image-1.jpg', publishdate:'2019-01-19T07:22Z', excert :'This is the summary'},
+      {id: 9, title:'the ninth author', author:'AD',image:'gallery-image-1.jpg', publishdate:'2019-01-19T07:22Z', excert :'This is the summary'},
+      {id: 10, title:'the tenth author', author:'AD',image:'gallery-image-1.jpg', publishdate:'2019-01-19T07:22Z', excert :'This is the summary'},
+
+    ]
+  }
+}
